ce367bf772fe10ef62d7abd7f1e1feaad1e55e08
Maria Smolina <mariia.smolina@gmail.com>
2025-01-30 10:38:34 +0200

    Create XAMPP_film.sql

---
+++ XAMPP_film.sql
@@ -0,0 +1,16 @@
+CREATE TABLE film(
+filmID int PRIMARY KEY AUTO_INCREMENT,
+filmNimetus varchar(30) not null,
+aasta int,
+eelarveHind decimal(7,1)
+);
+
+INSERT INTO film(filmNimetus, aasta, eelarveHind)
+VALUES('Riply', 2024, 22000.5),
+('Babygirl', 2024, 555555.5),
+('Barbie', 2023, 10000.7);
+
+ALTER TABLE film ADD zanrID int;
+
+ALTER TABLE film ADD CONSTRAINT fk_zanr
+FOREIGN KEY (zanrID) REFERENCES zanr(zanrID);
