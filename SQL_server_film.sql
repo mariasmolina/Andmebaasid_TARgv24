15b85aa700835ea440695aec363c9049024609d4
Maria Smolina <mariia.smolina@gmail.com>
2025-02-06 18:47:54 +0200

    Update SQL_server_film.sql

--- SQL_server_film.sql
+++ SQL_server_film.sql
@@ -15,11 +15,16 @@ DROP TABLE film;
 
 --andmete sisestamine tabelisse
 INSERT INTO film(filmNimetus, aasta, eelarveHind)
-VALUES(' ', 2024, 22000.5);
+VALUES('Riply', 2024, 22000.5),
+('Babygirl', 2024, 555555.5),
+('Barbie', 2023, 10000.7);
+(' ', 2024, 22000.5);
 
 --andmete kustutamine tabelist
 DELETE FROM film WHERE filmID=5;
 
+
+-----------------------------------
 CREATE TABLE zanr(
 zanrID int PRIMARY KEY identity(1,1),
 zanrNimetus varchar(20) UNIQUE
@@ -41,3 +46,50 @@ FOREIGN KEY (zanrID) REFERENCES zanr(zanrID);
 select * from film;
 select * from zanr;
 UPDATE film SET zanrID=2 WHERE filmID=3
+
+
+------------------------------------
+CREATE TABLE rezisoor(
+rez_ID int PRIMARY KEY identity(1,1),
+nimi varchar(20) UNIQUE,
+synniaeg DATE,
+riik varchar(25) not null
+)
+
+select * from rezisoor;
+
+INSERT INTO rezisoor(nimi, synniaeg, riik)
+VALUES('Smolina Maria', '1996-07-20', 'Estonia');
+
+ALTER TABLE film ADD rez_ID int;
+
+ALTER TABLE film ADD CONSTRAINT fk_rez
+FOREIGN KEY (rez_ID) REFERENCES rezisoor(rez_ID);
+
+UPDATE film SET rez_ID=2 WHERE filmID=2
+
+CREATE TABLE kinokava(
+kava_ID int PRIMARY KEY identity(1,1),
+kuupaev DATE,
+pileti_hind decimal(2,1),
+saal int
+);
+---kustutamine
+DROP TABLE kinokava;
+
+INSERT INTO kinokava(kuupaev, pileti_hind, saal)
+VALUES('2025-02-24', 6.9, 4);
+
+ALTER TABLE kinokava ADD filmID int;
+
+ALTER TABLE kinokava ADD CONSTRAINT fk_film
+FOREIGN KEY (filmID) REFERENCES film(filmID);
+
+UPDATE kinokava SET filmID=1 WHERE kava_ID=1
+
+ALTER TABLE film DROP COLUMN kava_ID;
+
+
+select * from film;
+select * from rezisoor;
+select * from kinokava;
