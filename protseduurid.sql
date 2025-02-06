2ae32e34ee36228bf50767195aae5fde52a18789
Maria Smolina <mariia.smolina@gmail.com>
2025-02-06 12:00:12 +0200

    Update protseduurid.sql

--- protseduurid.sql
+++ protseduurid.sql
@@ -103,15 +103,7 @@ INSERT INTO uudised(uudiseTeema, kuupaev, autor, kirjeldus)
 VALUES(
 'udune ilm', '2025-02-06', 'postimees', 'Lõunani on udune ilm')
 
---- 
-CREATE PROCEDURE `lisaUudis`(IN `uusTeema` VARCHAR(50), IN `paev` DATE, IN `autor` VARCHAR(25), IN `kirjeldus` TEXT) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER BEGIN
-
-INSERT INTO uudised(uudiseTeema, kuupaev, autor, kirjeldus)
-VALUES(
-uusTeema, paev, autor, kirjeldus);
-SELECT * FROM uudised;
-
-END;
-
+--protseduurite  kutse
 CALL lisaUudis ('windows 11', '2025-02-06', 'õpetaja Pant', 'w11 ei tööta multimeedia klassis');
----protseduuride
+
+--protseduurite screenid on read.me failis
